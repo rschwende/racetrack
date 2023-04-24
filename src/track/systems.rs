41b04cc669fa7f20e6204edaa72e114c714de5e3
rschwende <62968781+rschwende@users.noreply.github.com>
2023-04-23 22:34:30 -0700

    Update \track\system.rs
    Minor code clean up
    
    added 2D track example

--- src/track/systems.rs
+++ src/track/systems.rs
@@ -1,180 +1,104 @@
 use bevy::{prelude::*, render::{mesh::Indices, render_resource::PrimitiveTopology}, math::Vec4Swizzles};
-//use std::f32::consts::PI;
+use std::f32::consts::PI;
 
 use crate::track::components::*;
 
 pub const RAD_SUB_MAX_LEN:f32 = 0.3;    // max subdivision radial delta (ft)
 pub const ARC_SUB_MAX_LEN:f32 = 0.3;    // max subdivision arc length (ft)
 pub const MAX_CURVATURE: f32 = 3.;
-pub const PI: f32 = 3.14159265358979323846264338327950288f32;
 
-
-// pub fn spawn_test_scene(
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<StandardMaterial>>,
-// ) {
-//     // spawn a cube and a light
-//     commands.spawn(PbrBundle {
-//         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-//         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-//         transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-//         ..Default::default()
-//     });
-//     commands.spawn(PointLightBundle {
-//         transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-//         ..Default::default()
-//     });
-
-// }
-
-
-// fn setup(
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<StandardMaterial>>,
-// ) {
-//     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-
-//     // Positions of the vertices
-//     // See https://bevy-cheatbook.github.io/features/coords.html
-//     mesh.insert_attribute(
-//         Mesh::ATTRIBUTE_POSITION,
-//         vec![[0., 0., 0.], [1., 2., 1.], [2., 0., 0.]],
-//     );
-
-//     // In this example, normals and UVs don't matter,
-//     // so we just use the same value for all of them
-//     mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; 3]);
-//     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; 3]);
-
-//     // A triangle using vertices 0, 2, and 1.
-//     // Note: order matters. [0, 1, 2] will be flipped upside down, and you won't see it from behind!
-//     mesh.set_indices(Some(mesh::Indices::U32(vec![0, 2, 1])));
-
-//     commands.spawn(PbrBundle {
-//         mesh: meshes.add(mesh),
-//         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-//         ..default()
-//     });
-
-//     commands.spawn(PointLightBundle {
-//         point_light: PointLight {
-//             intensity: 1500.0,
-//             shadows_enabled: true,
-//             ..default()
-//         },
-//         transform: Transform::from_xyz(4.0, 8.0, 4.0),
-//         ..default()
-//     });
-
-//     commands.spawn(Camera3dBundle {
-//         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-//         ..default()
-//     });
-// }
-
-
-
-// pub fn spawn_CSYS(
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<StandardMaterial>>,
-// ) {
-
-//     let rotations = [
-//         Quat::from_rotation_x(0.0),
-//         Quat::from_rotation_x(-PI / 2.),
-//         Quat::from_rotation_z(PI / 2.),
-//     ];
-
-
-//     for rotation in rotations.into_iter() {
-
-//         commands.spawn(PbrBundle {
-//             mesh: meshes.add(Mesh::from(shape::Cylinder { radius: 0.05, height: 5.0, ..default() })),
-//             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
-//             transform: Transform::from_xyz(0., 0., 0.).with_rotation(rotation),
-//             ..default()
-//         });
-//     }
-
-
-// }
-
-//-----------------------------------------------------------------------------------------------------
-
+/// spawns a list of track elements
 pub fn spawn_track_element(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
 
-    // 0 
-    let transform0 = Transform::IDENTITY;
-    
-    // 1
-    let mut mesh1 = Mesh::new(PrimitiveTopology::TriangleList);
-    let mut transform1 = Transform::IDENTITY;
-    let track1 = TrackElement2D{
-        curvature: 0.05,
-        curve_angle: 90.,
-        width: 5.,
-        length: 10.,
-    };
+    // list of track elements
+    let track_list = vec![
+        // track element 1
+        TrackElement2D{
+            curvature: 0.1,
+            curve_angle: 90.,
+            width: 8.,
+            length: 10.,
+        }, 
 
-    let result = track_mesh_2d(track1, &mut mesh1, &mut transform1);
+        // track element 2
+        TrackElement2D{
+            curvature: 0.,
+            curve_angle: 90.,
+            width: 8.,
+            length: 20.,
+        }, 
 
-    // 2
-    let mut mesh2 = Mesh::new(PrimitiveTopology::TriangleList);
-    let mut transform2 = Transform::IDENTITY;
-    let track2 = TrackElement2D{
-        curvature: 0.,
-        curve_angle: 180.,
-        width: 5.,
-        length: 30.,
-    };
+        // track element 3
+        TrackElement2D{
+            curvature: -0.1,
+            curve_angle: 180.,
+            width: 8.,
+            length: 10.,
+        },
 
-    let result = track_mesh_2d(track2, &mut mesh2, &mut transform2);
+        // track element 4
+        TrackElement2D{
+            curvature: 0.,
+            curve_angle: 90.,
+            width: 8.,
+            length: 20.,
+        },
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(mesh1),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        transform: transform0,
-        ..default()
-    });
+        // track element 5
+        TrackElement2D{
+            curvature: 0.1,
+            curve_angle: 90.,
+            width: 8.,
+            length: 20.,
+        },
+    ];
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(mesh2),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        transform: transform1,
-        ..default()
-    });
+    let mut prev_transform = Transform::IDENTITY;
 
+    // iterate through track list and spawn to scene
+    for track in track_list {
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let mut new_transform = Transform::IDENTITY;
+        track_mesh_2d(track, &mut mesh, &mut new_transform);
+
+        commands.spawn(PbrBundle {
+            mesh: meshes.add(mesh),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            transform: prev_transform,
+            ..default()
+        });
+
+        prev_transform = prev_transform * new_transform;
+    }
+
+    // lighting
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(0.0, 8.0, 0.0),
+        transform: Transform::from_xyz(0.0, 0.0, 40.0),
         ..default()
     });
 
+    // test cube
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
         ..Default::default()
     });
 
 }
 
 
-
-
-/// 
-/// 
+/// creates track element mesh in passed mesh and passed transform ends as start position of
+/// next track element
 pub fn track_mesh_2d(track: TrackElement2D, track_mesh: &mut Mesh, transform: &mut Transform) -> bool {
     
     // vectors that define mesh
@@ -197,16 +121,13 @@ pub fn track_mesh_2d(track: TrackElement2D, track_mesh: &mut Mesh, transform: &m
     }
     
     // mesh density
-    //let num_arc_nodes = (length / ARC_SUB_MAX_LEN).ceil() as u32;
-    //let num_rad_nodes = (track.width / RAD_SUB_MAX_LEN).ceil() as u32;
+    let num_arc_nodes = (length / ARC_SUB_MAX_LEN).ceil() as u32;
+    let num_rad_nodes = (track.width / RAD_SUB_MAX_LEN).ceil() as u32;
 
-    let num_arc_nodes = 10;
-    let num_rad_nodes = 6;
-
-    //
+    // define vertices
     for curr_arc_node in 0..num_arc_nodes {
 
-        let curr_length = curr_arc_node as f32 / num_arc_nodes as f32 * length;
+        let curr_length = curr_arc_node as f32 / (num_arc_nodes - 1) as f32 * length;
 
         arc_transform(transform, curr_length, track.curvature);
         let matrix = transform.compute_matrix();
@@ -220,7 +141,7 @@ pub fn track_mesh_2d(track: TrackElement2D, track_mesh: &mut Mesh, transform: &m
             let p: Vec3 = (matrix * p).xyz();
 
             // define normals
-            let n = Vec4:: new (0., 0., 1., 0.);
+            let n = Vec4:: new (0., 0., 1., 1.);
             let n: Vec3 = (matrix * n).xyz();
 
             positions.push(p);
@@ -246,7 +167,6 @@ pub fn track_mesh_2d(track: TrackElement2D, track_mesh: &mut Mesh, transform: &m
     }
     
     // define mesh
-    //track_mesh = &Mesh::new(PrimitiveTopology::TriangleList);
     track_mesh.set_indices(Some(Indices::U32(indices)));
     track_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
     track_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
@@ -256,9 +176,8 @@ pub fn track_mesh_2d(track: TrackElement2D, track_mesh: &mut Mesh, transform: &m
 }
 
 
-
+/// creates transformation matrix for passed length along track element
 fn arc_transform(transform: &mut Transform, curr_length: f32, curvature: f32) {
-
 
     if curvature == 0. {
         // straight
@@ -269,8 +188,8 @@ fn arc_transform(transform: &mut Transform, curr_length: f32, curvature: f32) {
         // curve
         let theta = (curr_length * curvature).abs();
 
-        let x = theta.sin() / curvature;
-        let y = (1. - theta.cos()) / curvature;
+        let x = theta.sin() / curvature.abs();
+        let y = (1. - theta.cos()) / curvature.abs();
         let z = 0.;
 
         let translation: Transform;
@@ -278,12 +197,12 @@ fn arc_transform(transform: &mut Transform, curr_length: f32, curvature: f32) {
 
         if curvature > 0. {
             // left curve
-            translation = Transform::from_xyz(x, -y, z);
-            rotation = Transform::from_rotation(Quat::from_rotation_z(-theta));
-        } else {
-            // right curve
             translation = Transform::from_xyz(x, y, z);
             rotation = Transform::from_rotation(Quat::from_rotation_z(theta));
+        } else {
+            // right curve
+            translation = Transform::from_xyz(x, -y, z);
+            rotation = Transform::from_rotation(Quat::from_rotation_z(-theta));
         }
 
         *transform = Transform::IDENTITY * translation * rotation;
